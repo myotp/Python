63149b867bfb257b8d4414c6ec677c9e7bcdad5d
Jia Wang <wj2016@gmail.com>
2015-06-15 00:12:48 +0200

    [GAE Data Store] working sample app with db

--- DemoDataStore/main.py
+++ DemoDataStore/main.py
@@ -5,9 +5,19 @@ import os
 import jinja2
 import webapp2
 
+from google.appengine.ext import db
+from google.appengine.ext import ndb
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+# NDB cheat sheet: https://docs.google.com/document/d/1AefylbadN456_Z7BZOpZEXDq8cR8LYu7QgI7bt5V0Iw/mobilebasic
+# To save data in Google App Engine Data Store
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
 
 class MainHandler(webapp2.RequestHandler):
     def render_str(self, template, **params):
@@ -15,7 +25,9 @@ class MainHandler(webapp2.RequestHandler):
         return t.render(params)
 
     def render_front_page(self, title = "", art = "", error = ""):
-        s = self.render_str('front.html', title=title, art=art, my_error_str=error)
+        arts = db.GqlQuery('SELECT * FROM Art ORDER BY created DESC')
+
+        s = self.render_str('front.html', title=title, art=art, my_error_str=error, arts=arts)
         self.response.write(s)
 
     def get(self):
@@ -26,7 +38,10 @@ class MainHandler(webapp2.RequestHandler):
         art = self.request.get('art')
         print 'title: ', title, ' art: ', art
         if title and art:
-            self.response.write('Thanks')
+            # Save Art object to Data Store
+            a = Art(title=title, art=art)
+            a.put()
+            self.redirect('/')
         else:
             error_msg = "title and art"
             self.render_front_page(title=title, art=art, error=error_msg)
